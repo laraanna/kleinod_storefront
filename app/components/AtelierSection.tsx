453332449d30e14aa0ee93f62c63809064da6c6d
Lara <lara@laraannawagner.com>
2025-01-08 15:16:57 +0100

    Adjust atelier section design

--- app/components/AtelierSection.tsx
+++ app/components/AtelierSection.tsx
@@ -5,11 +5,11 @@ import {Image} from '@shopify/hydrogen';
 export function AtelierSection() {
   return (
     <section className="atelier-section--wrapper">
-      <div className="atelier-section-image--container">
-      </div>
+      <div className="atelier-section-image--container"></div>
       <div className="atelier-overlay--wrapper">
-        <Image src="https://cdn.shopify.com/s/files/1/0808/9255/9695/files/atelier-kleinod-title.svg?v=1736338618"  />
+        <Image src="https://cdn.shopify.com/s/files/1/0808/9255/9695/files/atelier-kleinod-title.svg?v=1736338618" />
       </div>
+      <p>Discover our materials and techniques </p>
     </section>
   );
 }
