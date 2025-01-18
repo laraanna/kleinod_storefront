e8972bde41272123a8c43b023bf8dd301f10f22d
Lara <lara@laraannawagner.com>
2025-01-18 23:55:02 +0100

    Update window referenc eonly for client side

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,7 +2,10 @@ import {RemixBrowser} from '@remix-run/react';
 import {startTransition, StrictMode} from 'react';
 import {hydrateRoot} from 'react-dom/client';
 
-if (!window.location.origin.includes('webcache.googleusercontent.com')) {
+if (
+  typeof window !== 'undefined' &&
+  !window.location.origin.includes('webcache.googleusercontent.com')
+) {
   startTransition(() => {
     hydrateRoot(
       document,
