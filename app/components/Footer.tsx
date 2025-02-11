b87db76124ce5984b0fb2d7ce24e6191f8abca72
Lara <lara@laraannawagner.com>
2025-02-11 17:55:23 +0100

    Update footer mobile responsive and add trademark

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,6 +3,7 @@ import {Await, NavLink} from '@remix-run/react';
 import type {FooterQuery, HeaderQuery} from 'storefrontapi.generated';
 import icon from '~/assets/logo-icon.svg';
 import {Image} from '@shopify/hydrogen';
+import useMediaQuery from '../helper/matchMedia';
 
 interface FooterProps {
   footer: Promise<FooterQuery | null>;
@@ -47,48 +48,67 @@ function FooterMenu({
   primaryDomainUrl: FooterProps['header']['shop']['primaryDomain']['url'];
   publicStoreDomain: string;
 }) {
+  const trademark = <p className="trademark">Atelier Kleinod © 2025</p>;
+  const isLargeScreen = useMediaQuery('(min-width: 45em)');
+
+  if (!menu) return null;
+
   return (
     <nav className="footer-menu" role="navigation">
-      {menu.items.map((item) => {
-        // Skip if no subitems
-        if (!item.items || item.items.length === 0) return null;
+      <div className="footer-menu--wrapper">
+        {menu.items.map((item, index) => {
+          // Skip if no subitems
+          if (!item.items || item.items.length === 0) return null;
 
-        return (
-          <div key={item.id} className="subitems-wrapper">
-            {item.items.map((subitem) => {
-              if (!subitem.url) return null;
+          return (
+            <div key={item.id} className="subitems-wrapper">
+              {item.items.map((subitem) => {
+                if (!subitem.url) return null;
 
-              // Strip domain if the URL is internal
-              const subitemUrl =
-                subitem.url.includes('myshopify.com') ||
-                subitem.url.includes(publicStoreDomain) ||
-                subitem.url.includes(primaryDomainUrl)
-                  ? new URL(subitem.url).pathname
-                  : subitem.url;
+                // Strip domain if the URL is internal
+                const subitemUrl =
+                  subitem.url.includes('myshopify.com') ||
+                  subitem.url.includes(publicStoreDomain) ||
+                  subitem.url.includes(primaryDomainUrl)
+                    ? new URL(subitem.url).pathname
+                    : subitem.url;
 
-              const isSubitemExternal = !subitemUrl.startsWith('/');
+                const isSubitemExternal = !subitemUrl.startsWith('/');
 
-              return isSubitemExternal ? (
-                <a
-                  href={subitemUrl}
-                  key={subitem.id}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {subitem.title}
-                </a>
-              ) : (
-                <NavLink end key={subitem.id} prefetch="intent" to={subitemUrl}>
-                  {subitem.title}
-                </NavLink>
-              );
-            })}
-          </div>
-        );
-      })}
-      <div className="logo--wrapper">
-        <Image src={icon} sizes="65px 65px" />
+                return (
+                  <>
+                    {isSubitemExternal ? (
+                      <a
+                        href={subitemUrl}
+                        key={subitem.id}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        {subitem.title}
+                      </a>
+                    ) : (
+                      <NavLink
+                        end
+                        key={subitem.id}
+                        prefetch="intent"
+                        to={subitemUrl}
+                      >
+                        {subitem.title}
+                      </NavLink>
+                    )}
+                  </>
+                );
+              })}
+              {index === 0 && isLargeScreen && trademark}
+            </div>
+          );
+        })}
+        <div className="logo--wrapper">
+          {!isLargeScreen && trademark}
+          <Image src={icon} width="65px" height="65px" />
+        </div>
       </div>
+      {/* <p>Atelier Kleinod © 2025</p> */}
     </nav>
   );
 }
