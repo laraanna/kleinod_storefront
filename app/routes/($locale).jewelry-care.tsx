af7706258ec1dbfab2ef6710e4d294dc69bb9963
Lara <lara@laraannawagner.com>
2025-03-03 10:06:47 +0100

    Add Jewelry Care page

---
+++ app/routes/($locale).jewelry-care.tsx
@@ -0,0 +1,9 @@
+export default function JewelryCare() {
+  return (
+    <div className="jewelry-care--container">
+
+      <p>Proper care helps maintain the beauty and longevity of your jewelry.</p>
+
+    </div>
+  );
+}
