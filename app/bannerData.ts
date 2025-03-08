5d6906feff0b4c68f54ce47553e1b9d3645794bb
Lara <lara@laraannawagner.com>
2025-03-08 11:47:53 +0100

    Update banner categories and styling

--- app/bannerData.ts
+++ app/bannerData.ts
@@ -1,16 +1,23 @@
 export const banner = [
   {
     id: 'all-rings',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/banner-saturn-signet.jpg?v=1736329216',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/banner-rings.jpg?v=1741429879',
     alt: 'all rings',
     title: 'All Rings',
     link: '/collections/all?category=rings',
   },
   {
     id: 'all-necklaces',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/lifestyle-test.jpg?v=1736416158',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/banner-necklaces.jpg?v=1741430035',
     alt: 'all necklaces',
     title: 'All Necklaces',
     link: '/collections/all?category=necklaces',
+  },
+  {
+    id: 'all-bracelets',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/banner-bracelets.jpg?v=1741429879',
+    alt: 'all bracelets',
+    title: 'All Bracelets',
+    link: '/collections/all?category=bracelets',
   },
 ];
