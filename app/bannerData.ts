2191f9de4e4cc5f8814ae0cba21567554f4da054
Lara <lara@laraannawagner.com>
2025-11-11 14:03:01 +0100

    Update landing carousel content

--- app/bannerData.ts
+++ app/bannerData.ts
@@ -1,28 +1,28 @@
 export const banner = [
   {
     id: 'all-rings',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/banner-rings.jpg?v=1741429879',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_dreiklang_3.jpg?v=1762786541',
     alt: 'all rings',
     title: 'All Rings',
     link: '/collections/all?category=rings',
   },
-  {
-    id: 'commission',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/bowtie_bespoke.jpg?v=1742146407',
-    alt: 'Bespoke',
-    title: 'Bespoke',
-    link: '/commission',
-  },
+  // {
+  //   id: 'commission',
+  //   url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/bowtie_bespoke.jpg?v=1742146407',
+  //   alt: 'Bespoke',
+  //   title: 'Bespoke',
+  //   link: '/commission',
+  // },
   {
     id: 'all-necklaces',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/banner-necklaces.jpg?v=1741430035',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_nubes_necklace_lifestyle_2.jpg?v=1762712760',
     alt: 'all necklaces',
     title: 'All Necklaces',
     link: '/collections/all?category=necklaces',
   },
   {
     id: 'all-bracelets',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/banner-bracelets.jpg?v=1741429879',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_nubes_bracelet_1.jpg?v=1762784876',
     alt: 'all bracelets',
     title: 'All Bracelets',
     link: '/collections/all?category=bracelets',
