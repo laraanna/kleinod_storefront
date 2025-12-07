823684add54b13dcbde29a03b71c3453d4e17ab6
Lara <lara@laraannawagner.com>
2025-12-07 14:08:32 +0100

    Update landing image selection

--- app/bannerData.ts
+++ app/bannerData.ts
@@ -1,7 +1,7 @@
 export const banner = [
   {
     id: 'all-rings',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_dreiklang_3.jpg?v=1762786541',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_butterfly_3.jpg?v=1764841906',
     alt: 'all rings',
     title: 'All Rings',
     link: '/collections/all?category=rings',
@@ -15,14 +15,14 @@ export const banner = [
   // },
   {
     id: 'all-necklaces',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_nubes_necklace_lifestyle_2.jpg?v=1762712760',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_evi_medaillon_lifestyle_2.jpg?v=1762785025',
     alt: 'all necklaces',
     title: 'All Necklaces',
     link: '/collections/all?category=necklaces',
   },
   {
     id: 'all-bracelets',
-    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_nubes_bracelet_1.jpg?v=1762784876',
+    url: 'https://cdn.shopify.com/s/files/1/0808/9255/9695/files/product_detail_nubes_bracelet_3.jpg?v=1764841854',
     alt: 'all bracelets',
     title: 'All Bracelets',
     link: '/collections/all?category=bracelets',
